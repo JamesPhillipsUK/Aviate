9a0261f281ef44af4888233a31d8c1e66936bbd5
James Phillips <JamesPhillipsUK@users.noreply.github.com>
2019-03-27 18:25:12 +0000

    Complete rewrite of Aviate.
    Moved legacy code to main.c.old for reference.
    Started rewrite of main.c.
    Updated README.md.
    Updated Changelog.

--- README.md
+++ README.md
@@ -1,24 +1,34 @@
-# Aviate
+# Aviate #
+
 Aviate is an ultra-lightweight text-editor designed for UNIX-like systems.
 
-## What is it?
+## What is it? #
+
 Aviate, the ultra-lightweight, single-script, terminal text-editor for UNIX-like systems.  Aviate is a recursive acronym, which stands for: Aviate Verifiably Is A Text Editor.  The Aviate Project started in April 2016, under the title MyProg.
 
 > MyProg is a working title.  It stands for My Program, because that's all it is.  Put simply, MyProg is so lightweight because it consists simply of a single script, written in C, which handles file I/O and text editing.  Put simply, once you have the software installed, navigate to the directory you want to use, then run the program.
 >  - Original README.md
- 
 
-## How do I install it?
-1. Download the version (Branch) you want from GitHub. 
-2. Open a terminal session in the myprog file.
-3. From your terminal, run "`$ sudo make`". 
-- N.B: Superuser or sudo priviliges are needed to decompile the software. 
+## How do I install it? #
 
-## How do I use it?
-Aviate works on a similar principle to other command-line tools you may be familiar with, such as GNU Nano in that you can call the software from inside any directory.  Once you open the program, you'll be asked where to save it.  If you want to, you can even choose to save it in a sub-directory.  Once you've chosen where to save it, just press enter to start editing.  To finish editing, double-tap [ENTER].
+1. Download the release you want from GitHub, I always suggest running the latest release.
+2. Open a terminal session in the Aviate file.
+3. From your terminal, run "`$ sudo make`".
+- N.B: Superuser or sudo priviliges are needed to decompile the software.
 
-## I've found a bug.  What do I do?
-I hope you don't have too many problems with Aviate, but as with everything in it's early stages there may be some teething problems.  If you do find any bugs, please report them, no matter how small.  You can report them in two ways, either: report them on GitHub, or report them directly to me at: [jamesphillipsuk.com/Contact](http://jamesphillipsuk.com/Contact/ "Contact me!").
+## How do I use it? #
 
-### My Website: 
-[jamesphillipsuk.com](http://jamesphillipsuk.com/ "My Website!").
+To use Aviate, follow these simple instructions, and you'll be on your way:
+
+1. To start writing a new file, use the command: "`Aviate Write x.y`" in your Bash terminal.
+2. To read or write to a pre-existing file, use: "`Aviate Read x.y`".
+3. Once Aviate has started, to cut your current line of text, use: `[CTRL]+K`.  To paste, use: `[CTRL]+U`.
+4. To save your work, use: `[CTRL]+O\`.  To Exit, use `[CTRL]+X`.
+
+## I've found a bug.  What do I do? ##
+
+I hope you don't have too many problems with Aviate, but as with everything in it's early stages there may be some teething problems.  If you do find any bugs, please report them as issues in the GitHub repo, no matter how small.
+
+### My Website: ###
+
+[jamesphillipsuk.com](https://jamesphillipsuk.com "My Website!").
--- main.c
+++ main.c
@@ -1,8 +1,8 @@
 /**
- * Aviate, created by James Phillips <james@jamesphillipsuk.com> on 3rd April 2016.  
+ * Aviate, created by James Phillips <james@jamesphillipsuk.com> 2016 - 2019.  
  * Aviate is a terminal-based text-editor, designed for *Nix systems.  
  * 
- * Copyright (C) 2016 James Phillips, Released under the GNU GPL v3.0 or later.
+ * Copyright (C) 2016, 2018 James Phillips, Released under the GNU GPL v3.0 or later.
  * 
  * This program is free software: you can redistribute it and/or modify it under the terms of the 
  * GNU General Public License as published by the Free Software Foundation, either version 3 of the License, 
@@ -12,332 +12,139 @@
  * You should have received a copy of the GNU General Public License along with this program.  
  * If not, see <http://www.gnu.org/licenses/>.
  *
- * Created on: 03/04/2016
- * Edited on: 03/04/2016
- * Edited on: 04/04/2016
- * Edited on: 05/04/2016
- * Edited on: 06/04/2016
- * Edited on: 09/04/2016
- * Edited on: 10/04/2016
- * Edited on: 07/06/2016
- * Edited on: 08/06/2016
- * Edited on: 11/06/2016
- * Edited on: 12/06/2016
- * Edited on: 13/06/2016
- * Edited on: 20/09/2016
- * Edited on: 21/09/2016
- * Edited on: 23/09/2016
- * Edited on: 26/09/2016
- * Edited on: 29/09/2016
- * Edited on: 01/10/2016
- * Edited on: 02/10/2016
- * Edited on: 03/10/2016 - 6 months in!
- * Edited on: 12/03/2017
+ * Initially created: April 2016
+ * Rewritten: December 2018 - March 2019
 **/
 
-#include <stdio.h>			/*I/O Library*/
-#include <stdlib.h>			/*Standard (File Handling) Library*/
-#include <string.h>			/*String Handling Library*/
-#include <errno.h>			/*Error Handling Library*/
+#include <stdio.h> /*I/O Library*/
+#include <stdlib.h>/*Standard (File Handling) Library*/
+#include <string.h>/*String Handling Library*/
+#include <errno.h>/*Error Handling Library*/
 
-#define CLEAR() printf("\e[2J\e[H");	/*output keycode to clear UNIX Terminal*/
-#define CLEARLN() printf("\e[2K\r");	/*Output keycode to clear one line in UNIX Terminal*/
+#define CLEAR() printf("\e[2J\e[H");/*output keycode to clear UNIX Terminal*/
+#define CLEARLN() printf("\e[2K\r");/*Output keycode to clear one line in UNIX Terminal*/
 
-int copyproc()				/*Displays copyright notice*/
+void printUIFrame()/*Prints the UI to the screen.  Title bar at the top, then top and bottom boundaries of the content area.*/
 {
-	CLEAR();
-	printf("╔══════════════════════════════════════════════════════════════════════════════╗\n");
-	printf("║                                    Aviate                                    ║\n");
-	printf("╚══════════════════════════════════════════════════════════════════════════════╝\n");
-	printf("╔══════════════════════════════════════════════════════════════════════════════╗\n");
-	printf("Aviate, created by James Phillips <james@jamesphillipsuk.com> on 3rd April 2016\n\nAviate is a terminal-based text-editor, designed for *Nix systems.\n\n");
-	printf("This program is free software: you can redistribute it and/or modify\nit under the terms of the GNU General Public License as published by\nthe Free Software Foundation, either version 3 of the License, or\n(at your option) any later version.\n\n");
-	printf("This program is distributed in the hope that it will be useful,\nbut WITHOUT ANY WARRANTY; without even the implied warranty of\nMERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the\nGNU General Public License for more details.\n\n");
-	printf("You should have received a copy of the GNU General Public License\nalong with this program.  If not, see <http://www.gnu.org/licenses/>.\n\n");
-	printf("Press ENTER to agree to these terms, or CTRL+BACKLASH to quit.");
-	printf("\033[24;0H╚══════════════════════════════════════════════════════════════════════════════╝");
-	return 0;
+  printf("╔══════════════════════════════════════════════════════════════════════════════╗\n");
+  printf("║                                    Aviate                                    ║\n");
+  printf("╚══════════════════════════════════════════════════════════════════════════════╝\n");
+  printf("╔══════════════════════════════════════════════════════════════════════════════╗\n");
+  printf("\033[24;0H╚══════════════════════════════════════════════════════════════════════════════╝");
+  printf("\033[5;0H");
 }
 
-int drawintroproc()				/*Draws design for the introductory screen*/
+void printUIIntroduction()/*This function prints an intro to Aviate to the screen.*/
 {
-	int Cnt = 0; /*Counter variable for for.. loop below.*/
-
-	CLEAR();
-	printf("╔══════════════════════════════════════════════════════════════════════════════╗\n");
-	printf("║                                    Aviate                                    ║\n");
-	printf("╚══════════════════════════════════════════════════════════════════════════════╝\n");
-	printf("╔══════════════════════════════════════════════════════════════════════════════╗\n");
-	for (Cnt = 0;Cnt < 19; ++ Cnt)	/*Loop: I'm too lazy to draw an 80 char x20 char box*/
-	{
-		printf("║                                                                              ║\n");
-	}
-	printf("╚══════════════════════════════════════════════════════════════════════════════╝");
-	printf("\033[5;2H Welcome to Aviate Public Alpha 2.2!\033[6;2H Aviate: the C-based text-editor designed for UNIX-like systems.\033[7;2H Designed and made by James Phillips.");
-	return 0;
+  printUIFrame();
+  printf("\033[5;26H Welcome to Aviate Alpha 3.0!\033[6;9H Aviate: the C-based text-editor designed for UNIX-like systems.\033[7;30H - by James Phillips.\033[8;19H Donate to me at: paypal.me/JamesPhillipsUK\033[23;27H Press [ENTER] to continue.");
+  getchar();
 }
 
-int drawmainproc() /*Draws the main page of the program*/
+void printHowToNotice()/*This function prints the instructions to the screen.*/
 {
-	int Cnt = 0;
-
-	CLEAR();
-	printf("\033[0;0H╔══════════════════════════════════════════════════════════════════════════════╗\n"); /*Draw box as in drawintroproc*/
-	printf("║                                    Aviate                                    ║\n");
-	printf("╚══════════════════════════════════════════════════════════════════════════════╝\n");
-	printf("╔══════════════════════════════════════════════════════════════════════════════╗\n");
-	for (Cnt = 0;Cnt < 19; ++ Cnt)
-	{
-		printf("                                                                                \n");
-	}
-	printf("╚══════════════════════════════════════════════════════════════════════════════╝");
-	printf("\033[5;0H");
-	return 0;
+  CLEAR();
+  printUIFrame();
+  printf("\033[5;26H Welcome to Aviate Alpha 3.0!\033[6;2H To use Aviate, follow these simple instructions, and you'll be on your way:\n 1| To start writing a new file, use the command: \"Aviate Write x.y\" in your\n     Bash terminal.\n 2| To read or write to a pre-existing file, use: \"Aviate Read x.y\".\n 3| Once Aviate has started, to cut your current line of text, use: \"[CTRL]+K\".\n     To paste, use: \"[CTRL]+U\".\n 4| To save your work, use: \"[CTRL]+O\".  To Exit, use \"[CTRL]+X\".\033[23;27H Press [ENTER] to continue.");
+  getchar();
 }
 
-int saveinitproc(char Src[1024], char Dest[1024]) /*Initial save, This creates the file to reserve it for later*/
+void printCopyrightNotice()/*This function prints the copyright/left notice to the screen.*/
 {
-	const char Pwd[] = "./\0";	/*Holds "./", UNIX present working directory*/
-	char Dir[] = "\0";		/*Will hold to user specified sub-directory and file name*/
-
-	printf("\033[7;2H Please enter the name of your file");
-	printf("\033[8;2H %s:", Pwd);
-	scanf("%s", Dir);
-	printf("\033[9;2H Saved to: %s%s", Pwd, Dir);
-
-	/*Code below saves file*/
-	strcpy(Src,  Dir);
-	strcpy(Dest, Pwd);
-	strcat(Dest, Src);
-
-	FILE *f = fopen(Dest, "w");
-	if (f == NULL)
-	{
-		printf("Error opening file: %s!\n", strerror(errno));
-		exit(1); /*Kill with exit code one if file cannot be created.*/
-	}
-	fprintf(f,"");
-	fclose(f);
-	printf("\033[11;26H Press [ENTER] to continue. ");
-	return 0;
+  CLEAR();
+  printUIFrame();
+  printf("\033[5;2H Copyleft Notice: \n");
+  printf("Aviate, created by James Phillips <james@jamesphillipsuk.com> on 3rd April 2016\n\n  Aviate is a terminal-based text-editor, designed for *Nix systems.\n\n  This program is free software: you can redistribute it and/or modify\n it under the terms of the GNU General Public License as published by\n the Free Software Foundation, either version 3 of the License, or\n (at your option) any later version.\n\n  This program is distributed in the hope that it will be useful,\n but WITHOUT ANY WARRANTY; without even the implied warranty of\n MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the\n GNU General Public License for more details.\n\n  You should have received a copy of the GNU General Public License\n along with this program.  If not, see <http://www.gnu.org/licenses/>.\033[23;25H Press [ENTER] to Exit.");
+  getchar();
 }
 
-int saveindevproc (char Dest[2048], char Buffer[]) /*Saves current buffer content to a .indev file*/
+void printFile (char (**fileNamePointer)[256])/*This prints the content of the file called by the user to the screen.*/
 {
-	char ReDest [2048] = "\0";
-
-	strcpy (ReDest, Dest);
-	strcat ( ReDest, ".indev");
-	FILE *f = fopen(ReDest, "a");
-	
-	if (f == NULL)
-	{
-		printf("Error opening file: %s!\n", strerror(errno));
-		exit(1);
-	}
-	fprintf(f, "%s", Buffer);
-
-	fclose(f);
-	return 0;
+  FILE *filePointer = fopen(*(fileNamePointer + 0 + 0)[0], "r");
+  char c;
+  if (filePointer == NULL)
+  {
+    printf("Error opening file: %s!\n", strerror(errno));
+    exit(0);
+  }
+  c = fgetc(filePointer);
+  while (c != EOF)
+  {
+    printf("%c", c);
+    c = fgetc(filePointer);
+  }
+  fclose(filePointer);
 }
 
-int savefinalproc (char Dest[2048]) /*Saves the final file when the editing is complete*/
+void rewriteFile(char (**fileNamePointer)[256])/*This allows the user to edit a pre-existing file.*/
 {
-	char ReDest [2048] = "\0";
-	char CharCopy;
-	int Remover;
-	strcpy (ReDest, Dest);
-	strcat ( ReDest, ".indev");
-	FILE *c = fopen (ReDest, "r");
-	FILE *f = fopen (Dest, "a");
-
-	if (f == NULL)
-	{
-		printf("Error opening file: %s!\n", strerror(errno));
-		exit(1);
-	}
-	if (c == NULL)
-	{
-		printf("Error opening file: %s!\n", strerror(errno));
-		exit(1);
-	}
-	while (1==1) /*infinitely copy content from the .indev file to the final file*/
-	{
-		CharCopy = fgetc (c);
-		if (CharCopy == EOF)
-		{
-			break;
-		}
-		else
-		{
-			fputc (CharCopy, f);
-		}
-	}
-	fclose(c);
-	fclose(f);
-	
-	Remover = remove(ReDest); /*Remove the .indev file, no longer needed for saving*/
-	if(Remover == 0) 
-	{
-		printf(".indev file successfully removed");
-	}
-	else 
-	{
-		printf("Error: unable to remove .indev file");
-	}
-
-	printf ("Success!\n%s\n%s", Dest, ReDest);
-	return 0;
+  /*TODO*/
 }
 
-int testupproc(char Buffer[], char Dest[2048],long int LineNo) /*Procedure to test for arrow key press*/
-{
-	int Cnt = 0;	/*Counter Variable for loop that reads through text input buffer*/
-	int Cnt1 = 0;	/*Counter variable for sub-procedures*/
-	char ReDest [2048] = "\0";	/*Overflow Buffer for file names*/
-	char ReReDest [2048] = "\0";
-	char CharRead;	/*Used in case A: to read characters within a file*/
-	long int FileLineNo = 0;	/*Used in case A: for checking the line number of a file*/
-	
-	for (Cnt = 0; Cnt < 2049; Cnt ++)
-	{
-		if (Buffer[Cnt] == '\033' && Buffer[Cnt+1] == '[')
- 		{
-			switch(Buffer[Cnt+2])
-			{
-				case 'A':
-				case 'D':
-					/*Up Arrow Press*/
-					Buffer[Cnt] = '\0'; /*Clear the current line*/
-					Buffer[Cnt+1] = '\0';
-					Buffer[Cnt+2] = '\0';					
-					CLEARLN();
-					printf("%s", Buffer); /*Printout the corrected buffer*/
-					printf("\033[1A"); /*Go up to active line then clear it*/
-					CLEARLN();
-					printf("\033[1A"); /*Go up to previous line then clear it*/
-					CLEARLN();
-					
-					strcpy (ReDest, Dest);
-					strcat (ReDest, ".indev");
-					FILE *f = fopen(ReDest, "r");
-					strcpy (ReReDest, ReDest);
-					strcat (ReReDest, ".tmp");
-					FILE *c = fopen(ReReDest, "w");
-					if (f == NULL || c == NULL)
-					{
-						printf("Error opening file: %s!\n", strerror(errno));
-						exit(1);
-					}
-					while (FileLineNo < (LineNo - 1)) /*copy content from the .indev file to temporary file*/
-					{
-						CharRead = fgetc (f);
-						if (CharRead == '\n')
-						{
-							FileLineNo = FileLineNo + 1;
-						}
-						else if (CharRead == EOF)
-						{
-							break; /*Break on end of file.*/
-						}
-						fputc (CharRead, c);
-					}
-					
-					fclose(c);
-					fclose(f);
-					
-					FILE *x = fopen(ReDest, "w+");
-					FILE *y = fopen(ReReDest, "r");
-					
-					while (1==1) /*copy new content from temporary file to the .indev file*/
-					{
-						CharRead = fgetc (y);
-						if (CharRead == EOF)
-						{
-							break; /*Emergency Break on end of file.  This shouldn't happen.*/
-						}
-						else
-						{
-							fputc (CharRead, x);
-						}
-					}
-					
-					fclose(y);
-					fclose(x);
-					
-					for (Cnt1 = 0; Cnt1 < 2049; Cnt1++ ) /*Clear the buffer*/
-					{
-						Buffer[Cnt1] = '\0';
-					}
-					break;
-				/*case 'D':
-					printf("\033[1D");
-					break;*/
-				case 'C':
-					/*Move to next line if on previous*/
-					
-					break;
-				case 'B':
-					/*Move forward one space if already back*/
-					
-					break;
-				default:
-					break;/*Break block if not an arrow key*/
-			}
-		testupproc(Buffer, Dest, LineNo); /*Revert to the start of the procedure*/
-		}
-	}
-	return 0;
+void writeFile(char (**fileNamePointer)[256])/*Writes an empty file.*/
+{/*TODO: check if the file exists before overwriting it.*/
+  FILE *filePointer = fopen(*(fileNamePointer + 0 + 0)[0], "a");
+
+  if (filePointer == NULL)
+  {
+    printf("Error opening file: %s!\n", strerror(errno));
+    exit(1);
+  }
+  fprintf(filePointer, "%c", '\0');
+
+  fclose(filePointer);
 }
 
-int textproc() /*Main input procedure for adding text to a file*/
+void handleReadWriteOrNothing(short readWriteOrNothingOutput, char (*fileNamePointer)[256])/*Handles what actions are taken when the user selects to read or write a file.*/
 {
-	unsigned ConsecutiveEnterCount = 0;
-	char Buffer [2048] = "\0";
-	char Src[1024], Dest[1024];	/*Used to set destination for saving*/
-	long int LineNo = 0;
-
-	saveinitproc(Src, Dest);
-	getchar();
-	drawmainproc();
-	for (;;)
-	{
-		
-		memset(Buffer, '\0', strlen(Buffer));
-		
-		if (fgets(Buffer, sizeof(Buffer), stdin) == NULL) /*Fgets reads string stream into buffer*/
-		{
-			break;
-		}
-		if (Buffer[0] == '\n')
-		{
-			ConsecutiveEnterCount++;
-			if (ConsecutiveEnterCount >= 1)
-			{
-				break;/*If enter is pressed twice, break the statement*/
-			}
-		}
-		else
-		{
-			testupproc(Buffer, Dest, LineNo);
-			LineNo = LineNo + 1;
-			ConsecutiveEnterCount = 0; 
-			saveindevproc(Dest, Buffer);
-		}
-	}
-	savefinalproc(Dest); /*Save the file*/
-	drawmainproc();
-	printf ("\033[10;25HThank you for using MyProg!\n\033[11;27HPress [ENTER] to close.");/*Closing message*/
-	return 0;
+  switch(readWriteOrNothingOutput)
+  {
+    case 1:/*No extra Args*/
+      printHowToNotice();
+      printCopyrightNotice();
+      break;
+    case 2:/*Read A File*/
+      printFile(&fileNamePointer);
+      rewriteFile(&fileNamePointer);
+      break;
+    case 3:/*Write a new File*/
+      writeFile(&fileNamePointer);
+      break;
+    default:/*Unknown Command*/
+      printf("Please provide your command in the format: \"Aviate <operation> <filename>\".  Exiting.\n");
+      break;
+  }
 }
 
-int main (void)
+short readWriteOrNothing(int *argc, char ***argv, char (*fileNamePointer)[256])/*Returns a value corresponding to whether the user wants to read or write a file.*/
 {
-	copyproc();
-	getchar();
-	drawintroproc();
-	textproc();
-	getchar();
-	CLEAR();
-	return 0;
+  short returnVal = 0;
+  if (*argc == 1)
+  {
+    for (int count = 0; count < 256; count++)
+      (*fileNamePointer)[count] = '\0';
+    returnVal = 1;
+  }
+  else if (*argc == 3)
+  {
+    if (strcmp(argv[0][1], "Read\0") == 0)
+      returnVal = 2;
+    else if (strcmp(argv[0][1], "Write\0") == 0)
+      returnVal = 3;
+    else
+      printf("%s is not a known command.  Try Read or Write.\n", argv[0][1]);
+    for (int count = 0; count < 256; count++)
+      (*fileNamePointer)[count] = argv[0][2][count];
+  }
+  return returnVal;
+}
+
+int main(int argc, char **argv)
+{
+  char fileName[256];
+
+  CLEAR();
+  printUIIntroduction();
+  handleReadWriteOrNothing(readWriteOrNothing(&argc, &argv, &fileName), &fileName);
+  return EXIT_SUCCESS;
 }
